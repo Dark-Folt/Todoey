5c8c65a30a92b4d4d37cb8144f581fbf8f72325d
Dark-Folt <marigo9794@gmail.com>
2018-12-10 15:48:58 +0100

    Convert to MVC design Pattern

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = [String]()
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard //Maintenant on peut utiliser userDefaults
 
@@ -19,7 +19,17 @@ class TodoListViewController: UITableViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+        let item = Item()
+        item.titile = "Find Mike"
+        
+        itemArray.append(item)
+
+        
+        let item1 = Item()
+        item1.titile = "Manger mon gars"
+
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
             itemArray = items
         }
         
@@ -35,7 +45,22 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.titile
+        
+        /*Ternary Operator ===>>
+         
+         valeur = condition ? valueIfTru : valueIfFalse
+         */
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+//
+//        if itemArray[indexPath.row].done == true {
+//            cell.accessoryType = .checkmark
+//        }else{
+//            cell.accessoryType = .none
+//        }
         
         return cell
     }
@@ -44,14 +69,12 @@ class TodoListViewController: UITableViewController {
     //MARK - TableViewDelegate Methods
     
    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
-   if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-        tableView.cellForRow(at: indexPath)?.accessoryType = .none
-   }else{
-    tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-    }
     
-        tableView.deselectRow(at: indexPath, animated: true)
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+
+        tableView.reloadData()
+        tableView.deselectRow(at: indexPath, animated: true) // faire disparaitre la selection grise
+    
     }
     
     //MARK - Add New Items
@@ -65,14 +88,16 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Ajouter l'item", style: .default) { (action) in
             // Quand l'user click le ajouter button
             
-            self.itemArray.append(alertTextField.text!)
+            let newItem = Item()
+            newItem.titile = alertTextField.text!
+            
+            self.itemArray.append(newItem)
 
             /*Je vais enregister mon tableau*/
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
 
             /*
              J'ai mis for key donc logement pour acceder dans cette base de donné je dois utiliser cette Key
-             
              Pour cela on va aller dans le view deadload ensuite cibler nos éléments
              */
             
