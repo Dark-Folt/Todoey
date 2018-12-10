a39cf9540d10dd2f26207962a29852e85e5afc42
Dark-Folt <marigo9794@gmail.com>
2018-12-10 14:52:23 +0100

    Data persisted using UserDefaults

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArrat = ["Find Mike","Buy Eggos","Destrpy Demogorgon"]
+    var itemArray = [String]()
+    
+    let defaults = UserDefaults.standard //Maintenant on peut utiliser userDefaults
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
         
         
     }
@@ -22,13 +29,13 @@ class TodoListViewController: UITableViewController {
     //MARK - TableView Datasource methodes
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArrat.count
+        return itemArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArrat[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row]
         
         return cell
     }
@@ -45,6 +52,44 @@ class TodoListViewController: UITableViewController {
     }
     
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var alertTextField = UITextField()
+        
+        let alert = UIAlertController(title: "Ajouter un Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Ajouter l'item", style: .default) { (action) in
+            // Quand l'user click le ajouter button
+            
+            self.itemArray.append(alertTextField.text!)
+
+            /*Je vais enregister mon tableau*/
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+
+            /*
+             J'ai mis for key donc logement pour acceder dans cette base de donné je dois utiliser cette Key
+             
+             Pour cela on va aller dans le view deadload ensuite cibler nos éléments
+             */
+            
+            self.tableView.reloadData()
+            
+        }
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Creer votre nouvel item"
+
+            alertTextField = textField
+            
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
     }
 
 
